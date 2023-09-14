d04fe011c2e792856c287fbc7c17fc60fb452831
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-09-14 09:40:38 -0300

    feat: add python basic types

---
+++ types.py
@@ -0,0 +1,12 @@
+year = 1989
+name = "Luke Skywalker"
+balance = 50.30
+calcule = 4j
+hasName = True
+
+
+print(("O tipo da variável year é {}".format(type(year))))
+print(("O tipo da variável name é {}".format(type(name))))
+print(("O tipo da variável balance é {}".format(type(balance))))
+print(("O tipo da variável calcule é {}".format(type(calcule))))
+print(("O tipo da variável hasName é {}".format(type(hasName))))
