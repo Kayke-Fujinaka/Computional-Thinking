4d6564e3bf47fa750414411f0c44473f9e7161d9
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-09-16 15:44:55 -0300

    refactor: heart beats exercise

--- heart_beats.py
+++ heart_beats.py
@@ -1,21 +1,28 @@
-age = int(input("Insira uma idade: "))
-
-if age <= 2:
-    print("Entre 120 a 140 BPM.")
-elif age <= 17:
-    print("Entre 80 a 100 BPM.")
-elif age >= 18:
-    is_sedentary = input("É sedentário? (sim/não): ").lower()
-    
-    while is_sedentary not in ['sim', 'não']:
-        print("Insira 'sim' ou 'não'.")
+def get_sedentary_response():
+    while True:
         is_sedentary = input("É sedentário? (sim/não): ").lower()
-      
-    if is_sedentary == 'sim':
-        print("Entre 70 a 80 BPM.")
-    else:
-        print("Entre 55 a 70 BPM.")
-else:
-    print("Entre 50 a 60 BPM.")
-  
-print()
+        if is_sedentary in ['sim', 'não']:
+            return is_sedentary == 'sim'
+        print("Insira 'sim' ou 'não'.")
+
+def get_bpm(age, is_sedentary=None):
+    if age <= 2:
+        return "Entre 120 a 140 BPM."
+    elif age <= 17:
+        return "Entre 80 a 100 BPM."
+    elif age >= 18:
+        if is_sedentary:
+            return "Entre 70 a 80 BPM."
+        return "Entre 55 a 70 BPM."
+
+def main():
+    age = int(input("Insira uma idade: "))
+    is_sedentary = None
+
+    if age >= 18:
+        is_sedentary = get_sedentary_response()
+        
+    print(get_bpm(age, is_sedentary))
+
+if __name__ == "__main__":
+    main()
