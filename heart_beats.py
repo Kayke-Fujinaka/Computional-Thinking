b9f85d81ac704510c846241c34e2e36a004c391a
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-09-16 16:08:04 -0300

    refactor: heart beats exercise

--- heart_beats.py
+++ heart_beats.py
@@ -1,28 +1,32 @@
-def get_sedentary_response():
+def input_sedentary_status():
     while True:
         is_sedentary = input("É sedentário? (sim/não): ").lower()
-        if is_sedentary in ['sim', 'não']:
-            return is_sedentary == 'sim'
+        if is_sedentary == 'sim':
+            return True
+        elif is_sedentary == 'não':
+            return False
         print("Insira 'sim' ou 'não'.")
 
-def get_bpm(age, is_sedentary=None):
+def determine_bpm(age, is_sedentary=False):
     if age <= 2:
         return "Entre 120 a 140 BPM."
     elif age <= 17:
         return "Entre 80 a 100 BPM."
-    elif age >= 18:
-        if is_sedentary:
-            return "Entre 70 a 80 BPM."
+    elif age >= 65:
+        return "Entre 50 a 60 BPM."
+    elif is_sedentary:
+        return "Entre 70 a 80 BPM."
+    else:
         return "Entre 55 a 70 BPM."
 
 def main():
     age = int(input("Insira uma idade: "))
-    is_sedentary = None
-
-    if age >= 18:
-        is_sedentary = get_sedentary_response()
-        
-    print(get_bpm(age, is_sedentary))
+    
+    if age >= 18 and age < 65:
+        is_sedentary = input_sedentary_status()
+        print(determine_bpm(age, is_sedentary))
+    else:
+        print(determine_bpm(age))
 
 if __name__ == "__main__":
     main()
